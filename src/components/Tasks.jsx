566eaebfd068213fa22a418b02f5794ec8b66404
rogerioplara <rogerioo.lara@hotmail.com>
2024-05-08 22:34:07 -0300

    usecallback, implementacao da busca

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,6 @@
-import { Checkbox, Table } from 'antd';
+import { Table, Input } from 'antd';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 // se o objeto for fixo, fazer ele fora da task instancia apenas uma vez, economiza processamento
 // se for dinamico precisa ser inserido no return
@@ -30,26 +30,62 @@ const columns = [
 const Tasks = () => {
   // cria uma lista de tarefas
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  // mais um exemplo de hook e useCallback
 
   // faz o request das tasks assíncrono, setando a lista para o response
-  const requestTasks = async () => {
+  // o useCallback é utilizado para que o HOOK não entre em loop, sendo chamado somente quando o valor (searchValue) for atualizado
+  const requestTasks = useCallback(async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos'
+        'https://jsonplaceholder.typicode.com/todos',
+        {
+          params: {
+            title: searchValue || undefined,
+          },
+        }
       );
       setTasks(response.data);
     } catch (err) {
       console.warn(err);
+    } finally {
+      setLoading(false);
     }
-  };
+  }, [searchValue]);
 
-  // chama a função do axios (fetch) e usa o effect
   useEffect(() => {
     requestTasks();
-  }, []);
+  }, [requestTasks]);
 
-  // retorna a tabela
-  return <Table dataSource={tasks} columns={columns} />;
+  // pega o evento do browser - no return será chamado no elemento input no onChange
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const renderContent = () => {
+    return (
+      <>
+        <Input
+          placeholder="Buscar tarefa por título"
+          onChange={handleChange}
+          value={searchValue}
+        />
+        <Table
+          dataSource={tasks}
+          columns={columns}
+          size="small"
+          loading={loading}
+          rowKey="id"
+          // rowKey={task => task.id}
+        />
+      </>
+    );
+  };
+
+  return <div>{renderContent()}</div>;
 };
 
 export default Tasks;
